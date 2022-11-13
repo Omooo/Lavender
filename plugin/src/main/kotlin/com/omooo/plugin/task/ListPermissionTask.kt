592f381f14e17f70e7b5488cd63df90becd0aebf
Omooo <869759698@qq.com>
2022-11-13 21:57:22 +0800

    opt ListPermissionTask code

--- plugin/src/main/kotlin/com/omooo/plugin/task/ListPermissionTask.kt
+++ plugin/src/main/kotlin/com/omooo/plugin/task/ListPermissionTask.kt
@@ -3,7 +3,6 @@ package com.omooo.plugin.task
 import com.android.build.gradle.api.BaseVariant
 import com.android.build.gradle.internal.api.ApplicationVariantImpl
 import com.android.build.gradle.internal.publishing.AndroidArtifacts
-import com.android.build.gradle.internal.tasks.CheckManifest
 import com.omooo.plugin.util.getArtifactName
 import com.omooo.plugin.util.writeToJson
 import org.gradle.api.DefaultTask
@@ -35,41 +34,32 @@ internal open class ListPermissionTask : DefaultTask() {
             """.trimIndent()
         )
 
-        val map = HashMap<String, List<String>>()
-
-        // 获取 app 模块的权限
-        val checkManifestTask = variant.checkManifestProvider.get() as CheckManifest
-        if (checkManifestTask.isManifestRequiredButNotPresent()) {
-            println("App manifest is missing for variant ${variant.name}")
-            getAppModulePermission(map)
-        } else {
-            val manifest = checkManifestTask.fakeOutputDir.asFile.get()
-            if (manifest.exists()) {
-                map["app"] = matchPermission(manifest.readText())
-            } else {
-                getAppModulePermission(map)
-            }
+        if (variant !is ApplicationVariantImpl) {
+            println("${variant.name} is not an application variant.")
+            return
         }
-
+        val resultMap = HashMap<String, List<String>>()
+        // 获取 app 模块的权限
+        getAppModulePermission(resultMap)
         // 获取 app 依赖的 aar 权限
-        val variantData = (variant as ApplicationVariantImpl).variantData
-        val manifests = variantData.variantDependencies.getArtifactCollection(
+        (variant as ApplicationVariantImpl).variantData.variantDependencies.getArtifactCollection(
             AndroidArtifacts.ConsumedConfigType.RUNTIME_CLASSPATH,
             AndroidArtifacts.ArtifactScope.ALL,
             AndroidArtifacts.ArtifactType.MANIFEST
-        )
-        val artifacts = manifests.artifacts
-        for (artifact in artifacts) {
-            if (!map.containsKey(artifact.getArtifactName())
-                && matchPermission(artifact.file.readText()).isNotEmpty()
-            ) {
-                map[artifact.getArtifactName()] = matchPermission(artifact.file.readText())
-            }
+        ).artifacts.asSequence().filter { artifact ->
+            !resultMap.containsKey(artifact.getArtifactName())
+                    && matchPermission(artifact.file.readText()).isNotEmpty()
+        }.forEach {
+            resultMap[it.getArtifactName()] = matchPermission(it.file.readText())
+        }.also {
+            resultMap.writeToJson("${project.parent?.projectDir}/permissions.json")
         }
 
-        map.writeToJson("${project.parent?.projectDir}/permissions.json")
     }
 
+    /**
+     * 获取 app 模块的权限信息
+     */
     private fun getAppModulePermission(map: HashMap<String, List<String>>) {
         val file = project.projectDir.resolve("src/main/AndroidManifest.xml")
         if (file.exists()) {
