81827421b46e68a916687ffadddb611202ce431d
Omooo <869759698@qq.com>
2023-03-16 10:35:53 +0800

    feat: 新增输出 Manifest 里定义的 scheme 任务

--- plugin/src/main/kotlin/com/omooo/plugin/task/ListSchemeVariantProcessor.kt
+++ plugin/src/main/kotlin/com/omooo/plugin/task/ListSchemeVariantProcessor.kt
@@ -2,9 +2,9 @@ package com.omooo.plugin.task
 
 import com.android.build.gradle.api.BaseVariant
 import com.android.build.gradle.internal.tasks.factory.dependsOn
-import com.omooo.plugin.spi.VariantProcessor
 import com.google.auto.service.AutoService
 import com.omooo.plugin.bean.LAVENDER
+import com.omooo.plugin.spi.VariantProcessor
 import com.omooo.plugin.util.nameCapitalize
 import com.omooo.plugin.util.processManifestTaskProvider
 import org.gradle.api.Project
@@ -12,32 +12,31 @@ import org.gradle.api.UnknownTaskException
 
 /**
  * Author: Omooo
- * Date: 2019/9/27
- * Version: v0.1.0
- * Desc: 注册 ListPermissionTask
- * @see ListPermissionTask
+ * Date: 2023/3/16
+ * Desc: 注册 [ListSchemeTask]
  */
 @AutoService(VariantProcessor::class)
-class ListPermissionVariantProcessor : VariantProcessor {
+class ListSchemeVariantProcessor : VariantProcessor {
 
     override fun process(project: Project, variant: BaseVariant) {
         val listPermissionsTask = try {
-            project.tasks.named("listPermissions")
+            project.tasks.named("listSchemes")
         } catch (e: UnknownTaskException) {
-            project.tasks.register("listPermissions") {
+            project.tasks.register("listSchemes") {
                 it.group = LAVENDER
-                it.description = "List the permission declared in AndroidManifest.xml"
+                it.description = "List the schemes declared in AndroidManifest.xml"
             }
         }
-        project.tasks.register("listPermissionsFor${variant.nameCapitalize()}", ListPermissionTask::class.java) {
+        project.tasks.register("listSchemesFor${variant.nameCapitalize()}", ListSchemeTask::class.java) {
             it.variant = variant
             it.group = LAVENDER
-            it.description = "List the permission declared in AndroidManifest.xml for ${variant.name}."
+            it.description = "List the schemes declared in AndroidManifest.xml for ${variant.name}."
             it.outputs.upToDateWhen { false }
         }.also {
             it.dependsOn(variant.processManifestTaskProvider)
             listPermissionsTask.dependsOn(it)
         }
+
     }
 
 }
