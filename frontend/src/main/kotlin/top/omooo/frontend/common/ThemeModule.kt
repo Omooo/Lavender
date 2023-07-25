0b9b97dad561800b4a2890801dd1acc94ddd8085
Omooo <869759698@qq.com>
2023-07-25 17:35:31 +0800

    feat: 增加 AarAnalyseTask 可视化输出

--- frontend/src/main/kotlin/top/omooo/frontend/common/ThemeModule.kt
+++ frontend/src/main/kotlin/top/omooo/frontend/common/ThemeModule.kt
@@ -10,7 +10,7 @@ typealias ThemeState = StateInstance<Theme>
 val ThemeContext = createContext<ThemeState>()
 
 val ThemeModule = FC<PropsWithChildren> { props ->
-    val state = useState(Themes.Dark)
+    val state = useState(Themes.Light)
     val (theme) = state
 
     ThemeContext(state) {
